3ac46a9fa83a6cd282298e78c3e80734c3ba358a
Yingjie Gu <guyingjie@fb.com>
2024-10-13 18:38:06 -0400

    Add Manual and function update

--- Apocalypse.py
+++ Apocalypse.py
@@ -15,11 +15,20 @@ pygame.display.set_caption("Apocalypse")
 # play background music
 mixer.music.load('/Users/guyingjie/Documents/albert_python_projects/Apocalypse/background.wav')
 mixer.music.play(-1)
+
+#create the weapon
+gun_width=2 
+gun_length=1
+
 # set player size
 left = 0
-top = 10
+top = 0
 length = 20
 width = 20
+human_x_ = width/2
+human_y_ = width/2
+human_radius_ = width/2
+
 rock_width_: int = width 
 rock_length_: int = length 
 diamond_width_ = width/2
@@ -70,7 +79,6 @@ clock = pygame.time.Clock()
 font_style = pygame.font.SysFont("bahnschrift", 50)
 score_style = pygame.font.SysFont("comicsansms", 35)
 
-
 # find right diamond position
 def locate_diamond():
     diamond_found = True
@@ -99,6 +107,18 @@ def hit_rock(human_left, human_top):
     return False
 
 
+def circle_hit_rock(radius_x, radius_y):
+    for each_rock in rock_location_:
+        rock_left = each_rock[0]
+        rock_top = each_rock[1]
+        rock_right = rock_left + each_rock[2]
+        rock_bottom = rock_top + each_rock[3]
+        if (radius_x >= rock_left and radius_x <= rock_right) and (
+            radius_y >= rock_top and radius_y <= rock_bottom):
+            return True
+    return False
+
+
 def hit_rock_by_rock(left, top, rock_location):
     for each_rock in rock_location:
         rock_left = each_rock[0]
@@ -111,7 +131,9 @@ def hit_rock_by_rock(left, top, rock_location):
     return False
 
 def hit_human_by_rock(rock_left, rock_top):
-    if left + 2*width >= rock_left and left - width < (rock_left + rock_width_) and top + 2*width >= rock_top and top - width < (
+    circle_external_square_left = human_x_ - width/2
+    circle_external_square_top = human_y_ - width/2
+    if circle_external_square_left + 2*width >= rock_left and circle_external_square_left - width < (rock_left + rock_width_) and circle_external_square_top + 2*width >= rock_top and circle_external_square_top - width < (
     rock_top + rock_length_):
         print("Hit human by rock. Relocating")
         return True
@@ -124,13 +146,13 @@ def draw_updated_rocks(rock_moving_counter, update_frames_per_second):
         rocky = rock_location[1]
         rock_health = rock_location[4]
         if rock_moving_counter % update_frames_per_second == 0:
-            if rockx > left:
+            if rockx >= human_x_:
                 rockx -= rock_moving_step
-            if rockx < left:
+            if rockx <= human_x_:
                 rockx += rock_moving_step
-            if rocky > top:
+            if rocky >= human_y_:
                 rocky -= rock_moving_step
-            if rocky < top:
+            if rocky <= human_y_:
                 rocky += rock_moving_step
             while hit_rock_by_rock(rockx, rocky, rock_location_[:i] if i >=1 else []):
                 # shift the rock randomly
@@ -194,7 +216,7 @@ while not exit_:
     pygame.draw.rect(canvas, red, [livex, livey, diamond_width_, diamond_length_])
     if powerx != 0 and powery != 0:
         pygame.draw.rect(canvas, yellow, [powerx, powery, diamond_width_, diamond_length_ ])
-    pygame.draw.rect(canvas, yellow, [left, top, width, length])
+    pygame.draw.circle(canvas, yellow, (human_x_, human_y_), human_radius_)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -205,31 +227,33 @@ while not exit_:
         pressed = pygame.key.get_pressed()
         moved = False
         # Hit boundary
-        if left > (screen_width - width) or left < 0 or top > (screen_width - width) or top < 0:
+        if human_x_ > (screen_width - width) or human_x_ < 0 or human_y_ > (screen_width - width) or human_y_ < 0:
             message("You lost. Thanks for playing Apocalypse", (255, 250, 130))
             pygame.display.update()
             Death_sound = mixer.Sound('explosion.wav')
             Death_sound.play()
-            left = 2
-            top = 2
+            human_x_ = 2
+            human_y_ = 2
             score_ = 0
             live_score_ = live_score_-1 if live_score_-1>0 else 0
-            bullet_score_ = 0 if live_score_ ==1 else bullet_score_
+            bullet_score_ = 0 if live_score_ == 0 else bullet_score_
+            weapon_power_up_level_ = 1 if live_score_ == 0 else weapon_power_up_level_
+            power_up_score_ = 0 if live_score_ == 0 else power_up_score_
             starttime = time.time()
             rock_location_ = []
             break
         # check key press
         if pressed[pygame.K_w]:
-            top -= human_moving_step
+            human_y_ -= human_moving_step
             moved=True
         elif pressed[pygame.K_s]:
-            top += human_moving_step
+            human_y_ += human_moving_step
             moved=True
         elif pressed[pygame.K_a]:
-            left -= human_moving_step
+            human_x_ -= human_moving_step
             moved=True
         elif pressed[pygame.K_d]:
-            left += human_moving_step
+            human_x_ += human_moving_step
             moved=True
         if moved:
             # draw current rocks
@@ -238,18 +262,20 @@ while not exit_:
             pygame.draw.rect(canvas, blue, [bulletx, bullety, diamond_width_, diamond_length_])
             pygame.draw.rect(canvas, red, [livex, livey, diamond_width_, diamond_length_])
             if powerx != 0 and powery != 0:
-                pygame.draw.rect(canvas, yellow, [powerx, powery, diamond_width_, diamond_length_])
-            pygame.draw.rect(canvas, yellow, [left, top, width, width])
+              pygame.draw.rect(canvas, yellow, [powerx, powery, diamond_width_, diamond_length_])       
+            pygame.draw.circle(canvas, yellow, (human_x_, human_y_), human_radius_)
             pygame.display.update()
             # check whether we hit the rock
-            if hit_rock(left, top):
+            if circle_hit_rock(human_x_, human_y_):
                 message("aaaahhh", red)
                 rock_location_ = []
-                left = 2
-                top = 2
+                human_x_ = 2
+                human_y_ = 2
                 score_ = 0
                 live_score_ = live_score_-1 if live_score_-1>0 else 0
-                bullet_score_ = 0 if live_score_ ==1 else bullet_score_
+                bullet_score_ = 0 if live_score_ == 0 else bullet_score_
+                weapon_power_up_level_ = 1 if live_score_ == 0 else weapon_power_up_level_
+                power_up_score_ = 0 if live_score_ == 0 else power_up_score_
                 starttime = time.time()
                 pygame.display.update()
                 Death_sound = mixer.Sound('explosion.wav')
@@ -257,19 +283,21 @@ while not exit_:
                 break
 
             # check whether we hit bullets or lives
-            if (bulletx >= left and bulletx <= (left + width)) and (bullety >= top and bullety <= (top + width)):
+            circle_external_square_left = human_x_ - width/2
+            circle_external_square_top = human_y_ - width/2
+            if (bulletx >= circle_external_square_left and bulletx <= (circle_external_square_left + width)) and (bullety >= circle_external_square_top and bullety <= (circle_external_square_top + width)):
                 print("Pick bullets(:!!")
                 bulletx, bullety = locate_diamond()
                 bullet_score_ += 10
                 bullet_sound = mixer.Sound('laser.wav')
                 bullet_sound.play()
-            if (livex >= left and livex <= (left + width)) and (livey >= top and livey <= (top + width)):
+            if (livex >= circle_external_square_left and livex <= (circle_external_square_left + width)) and (livey >= circle_external_square_top and livey <= (circle_external_square_top + width)):
                 print("Gain live(:!!")
                 livex, livey = locate_diamond()
                 live_score_ += 1
                 live_sound = mixer.Sound('laser.wav')
                 live_sound.play()
-            if (powerx >= left and powerx <= (left + width)) and (powery >= top and powery <= (top + width)):
+            if (powerx >= circle_external_square_left and powerx <= (circle_external_square_left + width)) and (powery >= circle_external_square_top and powery <= (circle_external_square_top + width)):
                 print("accumulating power up")
                 powerx, powery = 0, 0
                 power_up_score_ += 1
