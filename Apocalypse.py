6ccc2aa06bf3b5a3cd1af990a1f507b96d8a6f26
pptalbert <145158994+pptalbert@users.noreply.github.com>
2024-12-25 15:59:12 -0500

    Add a line weapon without rotations, and allow the player to kill zombies

--- Apocalypse.py
+++ Apocalypse.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 import random
 import time
@@ -13,7 +14,8 @@ canvas = pygame.display.set_mode((screen_width, screen_width))
 pygame.display.set_caption("Apocalypse")
 
 # play background music
-mixer.music.load('/Users/guyingjie/Documents/albert_python_projects/Apocalypse/background.wav')
+cwd=os.getcwd()
+mixer.music.load(f'{cwd}/background.wav')
 mixer.music.play(-1)
 
 #create the weapon
@@ -29,17 +31,17 @@ human_x_ = width/2
 human_y_ = width/2
 human_radius_ = width/2
 
-rock_width_: int = width 
-rock_length_: int = length 
+zombie_width_: int = width 
+zombie_length_: int = length 
 diamond_width_ = width/2
 diamond_length_= length/2
-rock_moving_step: int = 10
+zombie_moving_step: int = 10
 human_moving_step: int = 10
 update_frames_per_second_: int = 10
-rock_moving_counter_: int = 0
-# generate a rock every 15 seconds
-rock_generate_counter_: int = 15
-rock_health_: int = 20
+zombie_moving_counter_: int = 0
+# generate a zombie every 15 seconds
+zombie_generate_counter_: int = 15
+zombie_health_: int = 20
 
 # colorcode
 colorR = 255
@@ -53,13 +55,14 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 grey = (128, 128, 128)
 
-# initialize rock
-# list of rocks
-rock_location_ = []
+# initialize zombie
+# list of zombies with parameters
+# [left_top_x, left_top_y, width, length, health]
+zombie_location_ = []
 
 # store score
 score_ = 0
-# store the previous score that we have generated a rock. Without it, multiple rock_creator(True) will be called and multiple new rocks are created.
+# store the previous score that we have generated a zombie. Without it, multiple zombie_creator(True) will be called and multiple new zombies are created.
 prev_score_ = 0
 bullet_score_ = 0
 live_score_ = 1
@@ -92,95 +95,94 @@ def locate_diamond():
         return (diamondx, diamondy)
 
 
-def hit_rock(human_left, human_top):
-    for each_rock in rock_location_:
-        rock_left = each_rock[0]
-        rock_top = each_rock[1]
-        rock_right = rock_left + each_rock[2]
-        rock_bottom = rock_top + each_rock[3]
+def hit_zombie(human_left, human_top):
+    for each_zombie in zombie_location_:
+        zombie_left = each_zombie[0]
+        zombie_top = each_zombie[1]
+        zombie_right = zombie_left + each_zombie[2]
+        zombie_bottom = zombie_top + each_zombie[3]
         human_right = human_left + width
         human_bottom = human_top + length
         for corner_x, corner_y in [(human_left, human_top), (human_right, human_top), (human_left, human_bottom), (human_right,human_bottom)]:
-            if (corner_x >= rock_left and corner_x < rock_right) and (
-                    corner_y >= rock_top and corner_y < rock_bottom):
+            if (corner_x >= zombie_left and corner_x < zombie_right) and (
+                    corner_y >= zombie_top and corner_y < zombie_bottom):
                 return True
     return False
 
 
-def circle_hit_rock(radius_x, radius_y):
-    for each_rock in rock_location_:
-        rock_left = each_rock[0]
-        rock_top = each_rock[1]
-        rock_right = rock_left + each_rock[2]
-        rock_bottom = rock_top + each_rock[3]
-        if (radius_x >= rock_left and radius_x <= rock_right) and (
-            radius_y >= rock_top and radius_y <= rock_bottom):
+def circle_hit_zombie(radius_x, radius_y):
+    for each_zombie in zombie_location_:
+        zombie_left = each_zombie[0]
+        zombie_top = each_zombie[1]
+        zombie_right = zombie_left + each_zombie[2]
+        zombie_bottom = zombie_top + each_zombie[3]
+        if (radius_x >= zombie_left and radius_x <= zombie_right) and (
+            radius_y >= zombie_top and radius_y <= zombie_bottom):
             return True
     return False
 
 
-def hit_rock_by_rock(left, top, rock_location):
-    for each_rock in rock_location:
-        rock_left = each_rock[0]
-        rock_top = each_rock[1]
-        rock_right = rock_left + each_rock[2]
-        rock_bottom = rock_top + each_rock[3]
-        if (left >= rock_left and left <= rock_right) and (top >= rock_top and top <= rock_bottom):
-            print("Hit rock by rock. Relocating")
+def hit_zombie_by_zombie(left, top, zombie_location):
+    for each_zombie in zombie_location:
+        zombie_left = each_zombie[0]
+        zombie_top = each_zombie[1]
+        zombie_right = zombie_left + each_zombie[2]
+        zombie_bottom = zombie_top + each_zombie[3]
+        if (left >= zombie_left and left <= zombie_right) and (top >= zombie_top and top <= zombie_bottom):
             return True
     return False
 
-def hit_human_by_rock(rock_left, rock_top):
+def hit_human_by_zombie(zombie_left, zombie_top):
     circle_external_square_left = human_x_ - width/2
     circle_external_square_top = human_y_ - width/2
-    if circle_external_square_left + 2*width >= rock_left and circle_external_square_left - width < (rock_left + rock_width_) and circle_external_square_top + 2*width >= rock_top and circle_external_square_top - width < (
-    rock_top + rock_length_):
-        print("Hit human by rock. Relocating")
+    if circle_external_square_left + 2*width >= zombie_left and circle_external_square_left - width < (zombie_left + zombie_width_) and circle_external_square_top + 2*width >= zombie_top and circle_external_square_top - width < (
+    zombie_top + zombie_length_):
+        print("Hit human by zombie. Relocating")
         return True
     return False
 
-def draw_updated_rocks(rock_moving_counter, update_frames_per_second):
-    for i in range(len(rock_location_)):
-        rock_location = rock_location_[i]
-        rockx = rock_location[0]
-        rocky = rock_location[1]
-        rock_health = rock_location[4]
-        if rock_moving_counter % update_frames_per_second == 0:
-            if rockx >= human_x_:
-                rockx -= rock_moving_step
-            if rockx <= human_x_:
-                rockx += rock_moving_step
-            if rocky >= human_y_:
-                rocky -= rock_moving_step
-            if rocky <= human_y_:
-                rocky += rock_moving_step
-            while hit_rock_by_rock(rockx, rocky, rock_location_[:i] if i >=1 else []):
-                # shift the rock randomly
-                rockx += (round(random.randrange(-rock_width_, rock_width_)) / 10.0) * 10.0
-                rocky += (round(random.randrange(-rock_length_, rock_length_)) / 10.0) * 10.0
+def draw_updated_zombies(zombie_moving_counter, update_frames_per_second):
+    for i in range(len(zombie_location_)):
+        zombie_location = zombie_location_[i]
+        zombiex = zombie_location[0]
+        zombiey = zombie_location[1]
+        zombie_health = zombie_location[4]
+        if zombie_moving_counter % update_frames_per_second == 0:
+            if zombiex >= human_x_:
+                zombiex -= zombie_moving_step
+            if zombiex <= human_x_:
+                zombiex += zombie_moving_step
+            if zombiey >= human_y_:
+                zombiey -= zombie_moving_step
+            if zombiey <= human_y_:
+                zombiey += zombie_moving_step
+            while hit_zombie_by_zombie(zombiex, zombiey, zombie_location_[:i] if i >=1 else []):
+                # shift the zombie randomly
+                zombiex += (round(random.randrange(-zombie_width_, zombie_width_)) / 10.0) * 10.0
+                zombiey += (round(random.randrange(-zombie_length_, zombie_length_)) / 10.0) * 10.0
 
-            rock_location_[i][0]=rockx
-            rock_location_[i][1]=rocky
-        pygame.draw.rect(canvas, green if rock_health>rock_health_/2 else grey, [rockx, rocky, rock_width_, rock_length_])
+            zombie_location_[i][0]=zombiex
+            zombie_location_[i][1]=zombiey
+        pygame.draw.rect(canvas, green if zombie_health>zombie_health_/2 else grey, [zombiex, zombiey, zombie_width_, zombie_length_])
 
-def generate_rock():
+def generate_zombie():
     while (True):
-        rockx = round(random.randrange(0, (screen_width - rock_width_ - 1)) / 10.0) * 10.0
-        rocky = round(random.randrange(0, (screen_width - rock_length_ - 1)) / 10.0) * 10.0
-        if not hit_rock_by_rock(rockx, rocky, rock_location_) and not hit_human_by_rock(rockx, rocky):
+        zombiex = round(random.randrange(0, (screen_width - zombie_width_ - 1)) / 10.0) * 10.0
+        zombiey = round(random.randrange(0, (screen_width - zombie_length_ - 1)) / 10.0) * 10.0
+        if not hit_zombie_by_zombie(zombiex, zombiey, zombie_location_) and not hit_human_by_zombie(zombiex, zombiey):
             break
-    random_rock_health = round(random.randrange(1, rock_health_))
-    rock_location_.append([rockx, rocky, rock_width_, rock_length_, random_rock_health])
+    random_zombie_health = round(random.randrange(1, zombie_health_))
+    zombie_location_.append([zombiex, zombiey, zombie_width_, zombie_length_, random_zombie_health])
 
 
-def rock_creator(create_new, rock_moving_counter, update_frames_per_second):
-    # generate rock
-    # if rock_location is empty, draw the first rock.
-    # every time, redraw the rock in the rock_location.
-    # otherwise, wait until reaching score level and draw the next rock and put it in the rock_location.
-    if not rock_location_ or create_new:
-        generate_rock()
-    draw_updated_rocks(rock_moving_counter, update_frames_per_second)
+def zombie_creator(create_new, zombie_moving_counter, update_frames_per_second):
+    # generate zombie
+    # if zombie_location is empty, draw the first zombie.
+    # every time, redraw the zombie in the zombie_location.
+    # otherwise, wait until reaching score level and draw the next zombie and put it in the zombie_location.
+    if not zombie_location_ or create_new:
+        generate_zombie()
+    draw_updated_zombies(zombie_moving_counter, update_frames_per_second)
 
 
 def your_score():
@@ -195,6 +197,31 @@ def message(msg, color):
     mesg = font_style.render(msg, True, color)
     canvas.blit(mesg, [200, 500])
 
+def kill_zombie():
+    to_be_removed_zombie_index=[]
+    print(human_x_, human_y_)
+    for i in range(len(zombie_location_)): 
+        zombie_left = zombie_location_[i][0]
+        zombie_top = zombie_location_[i][1]
+        zombie_right = zombie_left + zombie_location_[i][2]
+        zombie_bottom = zombie_top + zombie_location_[i][3]
+        zombie_health = zombie_location_[i][4]
+        print(zombie_left, zombie_right,zombie_top, zombie_bottom, zombie_health)
+        if (zombie_left <= human_x_ and zombie_right >= human_x_) or (zombie_top <= human_y_ and zombie_bottom > human_y_):
+            zombie_health = zombie_health - zombie_health_/4
+            if zombie_health <= 0:
+                # health is gone, zombie should be removed
+                print(i)
+                to_be_removed_zombie_index.append(i)
+            zombie_location_[i][4] = zombie_health
+    # remove zombie from the zombie_location_, from tail to head
+    print(to_be_removed_zombie_index)
+    to_be_removed_zombie_index.reverse()
+    for idx in to_be_removed_zombie_index:
+        zombie_location_.pop(idx)
+
+
+
 
 # initialize diamonds
 bulletx, bullety = locate_diamond()
@@ -208,9 +235,9 @@ while not exit_:
     # refresh the canvas
     canvas.fill((0, 0, 0))
     score_ = your_score()        
-    # draw current rocks
-    rock_moving_counter_ += 1
-    rock_creator(False, rock_moving_counter_, update_frames_per_second_)
+    # draw current zombies
+    zombie_moving_counter_ += 1
+    zombie_creator(False, zombie_moving_counter_, update_frames_per_second_)
     # draw diamond
     pygame.draw.rect(canvas, blue, [bulletx, bullety, diamond_width_, diamond_length_])
     pygame.draw.rect(canvas, red, [livex, livey, diamond_width_, diamond_length_])
@@ -226,6 +253,7 @@ while not exit_:
         
         pressed = pygame.key.get_pressed()
         moved = False
+        shoot = False
         # Hit boundary
         if human_x_ > (screen_width - width) or human_x_ < 0 or human_y_ > (screen_width - width) or human_y_ < 0:
             message("You lost. Thanks for playing Apocalypse", (255, 250, 130))
@@ -240,7 +268,7 @@ while not exit_:
             weapon_power_up_level_ = 1 if live_score_ == 0 else weapon_power_up_level_
             power_up_score_ = 0 if live_score_ == 0 else power_up_score_
             starttime = time.time()
-            rock_location_ = []
+            zombie_location_ = []
             break
         # check key press
         if pressed[pygame.K_w]:
@@ -255,9 +283,12 @@ while not exit_:
         elif pressed[pygame.K_d]:
             human_x_ += human_moving_step
             moved=True
+        elif pressed[pygame.K_SPACE]: 
+            shoot = True
+        # take actions based on key press
         if moved:
-            # draw current rocks
-            rock_creator(False, rock_moving_counter_, update_frames_per_second_)
+            # draw current zombies
+            zombie_creator(False, zombie_moving_counter_, update_frames_per_second_)
             # draw diamond
             pygame.draw.rect(canvas, blue, [bulletx, bullety, diamond_width_, diamond_length_])
             pygame.draw.rect(canvas, red, [livex, livey, diamond_width_, diamond_length_])
@@ -265,10 +296,10 @@ while not exit_:
               pygame.draw.rect(canvas, yellow, [powerx, powery, diamond_width_, diamond_length_])       
             pygame.draw.circle(canvas, yellow, (human_x_, human_y_), human_radius_)
             pygame.display.update()
-            # check whether we hit the rock
-            if circle_hit_rock(human_x_, human_y_):
+            # check whether we hit the zombie
+            if circle_hit_zombie(human_x_, human_y_):
                 message("aaaahhh", red)
-                rock_location_ = []
+                zombie_location_ = []
                 human_x_ = 2
                 human_y_ = 2
                 score_ = 0
@@ -306,13 +337,31 @@ while not exit_:
                     weapon_power_up_level_ += 1
                 power_sound = mixer.Sound('laser.wav')
                 power_sound.play()
-    # draw new rock 
-    if score_ != 0 and (score_ % rock_generate_counter_ == 0 or score_ % power_up_block_generate_counter_ == 0) and score_ != prev_score_:
-        # without comparing prev_score and score_, multiple rock_creator(True) will be called and multiple new rocks are created. because the update_frames_per_second_ = 10, which means we will reach this location 10 times when score_ is multiplier of 20
+        if shoot:
+            # draw diamond
+            pygame.draw.rect(canvas, blue, [bulletx, bullety, diamond_width_, diamond_length_])
+            pygame.draw.rect(canvas, red, [livex, livey, diamond_width_, diamond_length_])
+            if powerx != 0 and powery != 0:
+                pygame.draw.rect(canvas, yellow, [powerx, powery, diamond_width_, diamond_length_])       
+            pygame.draw.circle(canvas, yellow, (human_x_, human_y_), human_radius_)
+            # draw shooting lines.
+            if bullet_score_ > 0:
+                bullet_score_ -= 10
+                pygame.draw.line(canvas, red, (human_x_,0), (human_x_, screen_width))
+                pygame.draw.line(canvas, red, (0, human_y_), (screen_width, human_y_))
+                # check whether we hit a zombie!
+                kill_zombie()
+            # draw current zombies
+            zombie_creator(False, zombie_moving_counter_, update_frames_per_second_)
+            pygame.display.update()
+            
+    # draw new zombie 
+    if score_ != 0 and (score_ % zombie_generate_counter_ == 0 or score_ % power_up_block_generate_counter_ == 0) and score_ != prev_score_:
+        # without comparing prev_score and score_, multiple zombie_creator(True) will be called and multiple new zombies are created. because the update_frames_per_second_ = 10, which means we will reach this location 10 times when score_ is multiplier of 20
         prev_score_ = score_
-        if score_ % rock_generate_counter_ == 0:
-            # generate a new rock
-            rock_creator(True, rock_moving_counter_, update_frames_per_second_)
+        if score_ % zombie_generate_counter_ == 0:
+            # generate a new zombie
+            zombie_creator(True, zombie_moving_counter_, update_frames_per_second_)
         elif score_ % power_up_block_generate_counter_ == 0:
             # generate a new power up block
             if powerx == 0 and powery == 0:
